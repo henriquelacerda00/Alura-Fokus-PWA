a9c9d7f0bd26f6fe9c1fa5809121b02d9a2a2c1c
Henrique Lacerda <henriquelacerda69@gmail.com>
2025-07-14 12:55:22 -0300

    Adiciona script build:netlify e arquivo _redirects para rotas SPA

--- ng-fokus-main/dist/ng-fokus/server/main.server.mjs
+++ ng-fokus-main/dist/ng-fokus/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-F5WVPU3L.mjs";import"./chunk-YQP63HMD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RHEECYRW.mjs";import"./chunk-YQP63HMD.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
